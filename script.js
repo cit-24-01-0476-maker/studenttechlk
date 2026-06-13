39351152e4b0fe7a149517f2c8a2c62b6341d28b
S.A.Kavindu Oshadha Perera <oshadhaperera504@gmail.com>
2026-06-14 00:50:46 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,13 +2,3 @@ function toggleMenu() {
   const navMenu = document.getElementById("navMenu");
   navMenu.classList.toggle("show");
 }
-
-function subscribeUser(event) {
-  event.preventDefault();
-
-  const email = document.getElementById("emailInput").value;
-
-  alert("Thank you! " + email + " subscribed successfully.");
-
-  document.getElementById("emailInput").value = "";
-}
